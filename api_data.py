f1561a3d3398e2618ecdbdb7de98f3d9e5c1e2d3
Deekshith <140245640+DeekshithPoojary98@users.noreply.github.com>
2024-06-24 18:06:25 +0530

    Update api_data.py

--- api_data.py
+++ api_data.py
@@ -1,4 +1,5 @@
 import random
+from endpoints import *
 
 username = "DEEKSHITH"
 password = "Sirma@123"
@@ -9,14 +10,22 @@ pdf_file_name = "sample.pdf"
 pdf_page_count = 10
 updated_file_name = "UpdatedTestingFile"
 updated_folder_name = folder_name + "_updated"
+document_password = "123456"
 old_tag = "OldTag"
 new_tag = "NewTag"
 new_remarks = "NewRemarks"
 pdf_split_range = "1-5"
 operation_types = [("range", "5-8", 4), ("random", "1,5,9", 3), ("single", "4", 1)]
 actions = ['favourite', 'archive', 'trash']
-file_formats = ["jpeg", "png", "zip", "doc", "txt", "docx", "xlsx", "tiff"]
+file_formats = ["jpeg", "png", "doc", "txt", "docx", "xlsx", "tiff"]
 search_filters = ["isEqualTo", "contains", "beginsWith", "endsWith", "notContains"]
+workspace_field_names = ['blank_foldername', 'blank_filepath', 'character_limit', 'file_already_exists']
+file_upload_field_names = ['blank_filepath', 'blank_foldername', 'blank_tag', 'blank_files', 'blank_remarks', 'invalid_workspacetype']
+workspaces = ["AUTOWORKSPACE", "MANUALWORKSPACE"]
+password_validations = {
+    "correct_password": (document_password, True),
+    "wrong_password": ("123", False)
+}
 
 test_create_manualworkspace_directory_json = {
     "folderPath": f"/IDOC_Filesystem/workspace/manualworkspace/{folder_name}",
@@ -89,6 +98,29 @@ test_update_file_tags_json = {
 test_update_file_remark_json = {
     "id": None,
     "remarks": new_remarks
+}
+
+test_update_file_is_verified_json = {
+    "id": None,
+    "isVerified": 'true'
+}
+
+
+test_update_file_password_json = {
+    "id": None,
+    "protectedPassword": "true",
+    "password": document_password
+}
+
+test_update_file_all_fields = {}
+
+test_audit_params = {
+    "id": None
+}
+
+test_check_password_json = {
+    "id": None,
+    "password": None
 }
 
 test_make_file_fav_arch_trs_json = {
@@ -169,4 +201,24 @@ test_merge_pdfs_params = {
 
 test_download_zip_params = {
     "documentIds": None
+}
+
+api_data = {
+    "create_manualworkspace_directory_api": {
+        "url": create_directory_url,
+        "data": test_create_manualworkspace_directory_json
+    },
+    "upload_file_api": {
+        "url": upload_url,
+        "data": test_upload_data
+    }
+}
+
+test_cases = {
+    "blank_foldername": ("folderName", "", "Folder name is required"),
+    "blank_filepath": ("filePath", "", "File path is required"),
+    "blank_tag": ("tags", "", "At least one tag must be provide"),
+    "blank_files": ("files", [], "Files cannot be null"),
+    "blank_remarks": ("remarks", "", "Remarks are required"),
+    "invalid_workspaceType": ("workspaceType", "InvalidWorkSpace", "Invalid workspaceType field")
 }
